9af9f3a3c6927010fc4cd54e069415665f25debb
LFS Luis Silva <147883078+Luisdelvitis@users.noreply.github.com>
2024-09-02 22:00:09 -0300

    Add files via upload

---
+++ Exercicio 02.py
@@ -0,0 +1,8 @@
+def eh_par(num):
+
+  if num % 2 == 0:
+      return "par"
+      else:
+      return "impar"
+  
+print(eh_par(3))
---
+++ Exercicio 03.py
@@ -0,0 +1,6 @@
+def reverter_string(string):
+    stringInvertida = string[::-1]
+      
+       return stringInvertida
+
+print(reverter_string("Lucas"))
